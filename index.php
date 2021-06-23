ce9ce7755525f11616adea798b263fc4c13b2549
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-22 21:10:44 -0500

    initialization of database is set up

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="">
+
+                <input type="text" name="title" placeholder="This field is required" />
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
+            </form>
+        </div>
+        <div class="show-todo-section">
+            <div class="todo-item">
+                <input type="checkbox">
+                <h2>This is hhsx nknak</h2>
+                <br />
+                <small>created 6/22/2022</small>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
